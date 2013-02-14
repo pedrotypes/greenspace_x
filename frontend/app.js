b5f448c11b51ab343cc105a8f9e8254251ff063b
Pedro Candeias <pgscandeias@gmail.com>
2013-02-14 13:32:23 +0000

    Have the node app see the Bases collection

--- frontend/app.js
+++ frontend/app.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('localhost', 'stargame');
+var db = mongoose.connection;
+
+var baseSchema = mongoose.Schema({
+    name: String,
+    size: Number,
+    money: Number
+}, { collection: 'base'});
+var Base = mongoose.model('Base', baseSchema);
 
 app.get('/', function(req, res) {
-    var body = 'STAR GAME HOLLAAAA';
-    res.send(body);
+    Base.find(function(err, bases) {
+        var out = [];
+
+        bases.forEach(function(doc) {
+            out.push(new Base(doc));
+        });
+
+        res.send(out);
+    });
 });
 
 port = 80;
