f10ca95cb5c45d9edc35cffc020a2c693cdbd895
Pedro Candeias <pgscandeias@gmail.com>
2013-02-14 15:50:09 +0000

    Install jade

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var app = express();
+
+app.configure(function() {
+    app.use(express.static('/static', __dirname + '/public'));
+    app.engine('.html', require('jade').__express);
+    app.set('view engine', 'jade');
+});
+
 var mongoose = require('mongoose');
 mongoose.connect('localhost', 'stargame');
 var db = mongoose.connection;
@@ -7,11 +14,18 @@ var db = mongoose.connection;
 var baseSchema = mongoose.Schema({
     name: String,
     size: Number,
-    money: Number
+    money: Number,
+    x: Number,
+    y: Number
 }, { collection: 'base'});
 var Base = mongoose.model('Base', baseSchema);
 
+
 app.get('/', function(req, res) {
+    res.render('index', {title: 'cenas'});
+});
+
+app.get('/bases', function(req, res) {
     Base.find(function(err, bases) {
         var out = [];
 
