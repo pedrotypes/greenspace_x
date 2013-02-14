166443e18154d64a009bcd61a232feabbb0419a1
Pedro Candeias <pgscandeias@gmail.com>
2013-02-14 17:49:35 +0000

    Show each base's name

--- frontend/public/main.js
+++ frontend/public/main.js
@@ -4,11 +4,14 @@ $(function() {
         $.each(data, function(i, base) {
             var tpl = Handlebars.compile($("#tpl-base").html());
             var radius = Math.round(base.size / 2);
+            var base_margin = Math.round(10 - radius) / 2;
             map.append(tpl({
-                top: base.y * 10 - radius,
-                left: base.x * 10 - radius,
-                width: radius,
-                height: radius
+                area_top: base.y * 10 - 5,
+                area_left: base.x * 10 - 5,
+                base_margin: base_margin,
+                base_width: radius,
+                base_height: radius,
+                base: base
             }));
         });
     });
